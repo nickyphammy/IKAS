9a9758549461b0ddbbc889868d7f2f6fb99a9ce1
Nicholas Pham <nlpham1@uci.edu>
2026-06-01 19:00:32 -0700

    feat: fixed the explore page to show the map.

--- src/components/explore/ExploreMap.tsx
+++ src/components/explore/ExploreMap.tsx
@@ -1,8 +1,14 @@
+import { asset } from '../../lib/asset'
 import ExploreRadiusTool from './ExploreRadiusTool'
 
 export default function ExploreMap() {
   return (
     <div className="absolute inset-0">
+      <img
+        src={asset('assets/explore/map.png')}
+        alt="Map of viewpoints"
+        className="size-full object-cover object-center"
+      />
       <ExploreRadiusTool />
     </div>
   )
